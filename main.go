77bbfaa1ed2604eba0157f04c45734cee8416870
Sam Brougham <sam.brougham@gmail.com>
2022-11-23 15:19:34 -0500

    Change the filesystem

--- main.go
+++ main.go
@@ -1,8 +1,8 @@
 package main
 
 import (
-  "fmt"
   "os"
+  "fmt"
   "os/exec"
   "syscall"
 )
@@ -19,29 +19,39 @@ func main() {
 }
 
 func parent() {
+  // The first argument refers to the currently-executing program, so
+  // this command self-executes the program with "child" as an argument,
+  // which provides an isolated UTS (hostname/domain name) namespace
   cmd := exec.Command("/proc/self/exe", append([]string{"child"}, os.Args[2:]...)...)
+
   cmd.Stdin = os.Stdin
   cmd.Stdout = os.Stdout
   cmd.Stderr = os.Stderr
 
+  // Create our own UTS, PID and MNT namespaces
   cmd.SysProcAttr = &syscall.SysProcAttr{
     Cloneflags: syscall.CLONE_NEWUTS | syscall.CLONE_NEWPID | syscall.CLONE_NEWNS,
   }
-  
+
   if err := cmd.Run(); err != nil {
-    fmt.Println("ERROR", err)
+    fmt.Println("Error running parent command:", err)
     os.Exit(1)
   }
 }
 
 func child() {
+  syscall.Sethostname([]byte("container"))
+  must(syscall.Chdir("/"))
+  // Mount a new filesystem; see https://man7.org/linux/man-pages/man2/mount.2.html
+  must(syscall.Mount("proc", "proc", "proc", 0, ""))
+
   cmd := exec.Command(os.Args[2], os.Args[3:]...)
   cmd.Stdin = os.Stdin
   cmd.Stdout = os.Stdout
   cmd.Stderr = os.Stderr
 
   if err := cmd.Run(); err != nil {
-    fmt.Println("ERROR", err)
+    fmt.Println("Error running child command:", err)
     os.Exit(1)
   }
 }
